8a4b11725e93b06d62de8f1381321b6cd2fdf89d
malikmuskan <malikmuskan152009@gmail.com>
2022-10-13 00:08:54 -0400

    commit

--- app/src/main/java/com/example/mycountdownapp/MainActivity.kt
+++ app/src/main/java/com/example/mycountdownapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mycountdownapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.widget.Button
 import android.widget.Chronometer
 
@@ -11,47 +12,80 @@ import android.widget.Chronometer
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var countdown:Chronometer
+    lateinit var countdown: Chronometer
     var running = false
     var offset: Long = 0
+    val offset_key = "offset"
+    val running_key = "running"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        countdown = findViewById<Chronometer>(R.id.countdown)
+        countdown = findViewById<Chronometer>(R.id.chronometer)
+        if(savedInstanceState!= null) {
+            offset = savedInstanceState.getLong(offset_key)
+            running = savedInstanceState.getBoolean(running_key)
 
-         var restartbtn = findViewById<Button>(R.id.restartbtn)
-        restartbtn.setOnClickListener {
-            if(!running) {
+
+            setTimer(0)
+            if (running) {
+                countdown.start()
+            }
+        }
+        var one_sec = findViewById<Button>(R.id.one_sec)
+        one_sec.setOnClickListener {
+            Log.v("button click", " add 1 sec")
+            setTimer(1000)
+
+        }
+        val five_sec = findViewById<Button>(R.id.five_sec)
+        five_sec.setOnClickListener {
+            Log.v("button click", "add 5 sec")
+            setTimer(5000)
+        }
+        val ten_sec = findViewById<Button>(R.id.ten_sec)
+        ten_sec.setOnClickListener {
+            Log.v("button click", "add 10 sec")
+            setTimer(100000)
+
+
+        }
+        val start = findViewById<Button>(R.id.start)
+        start.setOnClickListener {
+            if (!running) {
                 setBasetime()
-                countdown.restart()
-                offset= 0
+                countdown.start()
                 running = true
             }
-        }
-        var plusbtn = findViewById<Button>(R.id.plusbtn)
-        plusbtn.setOnClickListener{
-            if(running){
-                saveOffset()
-                countdown.plus()
-                offset = 0
-                running=true
 
+        }
+        val stop = findViewById<Button>(R.id.stop)
+        stop.setOnClickListener {
+            if (running) {
+                saveOffset()
+                countdown.stop()
+                running = false
             }
         }
-        var incbtn = findViewById<Button>(R.id.incbtn)
-        incbtn.setOnClickListener{
-            if(running)
-                countdown.inc()
-            running= true
+        val restart = findViewById<Button>(R.id.stop)
+        restart.setOnClickListener {
+            offset = 0
+            setBasetime()
         }
+    }
 
-
+    private fun setTimer(value:Long) {
+        offset +=value
+        countdown.base = SystemClock.elapsedRealtime() - offset
 
     }
 
-
+    fun saveOffset(){
+        var offset = SystemClock.elapsedRealtime() - offset
+        TODO("Not yet implemented")
+    }
 
     private fun setBasetime() {
         TODO("Not yet implemented")
@@ -59,21 +93,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-private fun Chronometer.inc() {
-    TODO("Not yet implemented")
-}
-
-
-    private fun saveOffset() {
-        var offset: Long = SystemClock.elapsedRealtime()
-        TODO("Not yet implemented")
-    }
-
-
-    private fun Chronometer.plus() {
-    TODO("Not yet implemented")
-}
-
-private fun Chronometer.restart() {
-    TODO("Not yet implemented")
-}
